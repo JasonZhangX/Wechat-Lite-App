a7c24da63965ea29e584e34a540cf19c180f8aed
Jason Zhang <v-zhlong@microsoft.com>
2017-03-16 13:35:59 +0800

    Add new page.
    Add service and history page.

--- code/pages/index/index.js
+++ code/pages/index/index.js
@@ -2,26 +2,12 @@
 //获取应用实例
 var app = getApp()
 Page({
-data: {
-    item: {
-      index: 0,
-      msg: 'this is a template',
-      time: '2016-09-15'
-    }
-  },
-  add: function(e) {
-    this.setData({
-      count: this.data.count + 1
-    })
+  data: {
+    status: "OK",
+    title: "服务正常运行"
   },
   onLoad: function () {
     console.log('onLoad')
 
-  },
-  viewTap: function () {
-    console.log('view tap')
-  },
-   tapName: function(event) {
-    console.log(event)
   }
 })
