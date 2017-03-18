1bc7e445c2d8f4ae3f27cf4c65e5452fda7899f0
Jason Zhang <v-zhlong@microsoft.com>
2017-03-18 16:46:28 +0800

    Add history list page.
    Add history list page.

--- code/pages/history/history.js
+++ code/pages/history/history.js
@@ -1,3 +1,82 @@
 Page({
-    
+    data: {
+        history: [{
+            id: '2017',
+            date: '2017 03-02',
+            service: 'Virtual Machines',
+            area: '中国北区'
+        },
+        {
+            id: '2017',
+            date: '2017 03-02',
+            service: 'Virtual Machines',
+            area: '中国北区'
+        },
+        {
+            id: '2017',
+            date: '2017 03-02',
+            service: 'Virtual Machines',
+            area: '中国北区'
+        },
+        {
+            id: '2017',
+            date: '2017 03-02',
+            service: 'Virtual Machines',
+            area: '中国北区'
+        },
+        {
+            id: '2017',
+            date: '2017 03-02',
+            service: 'Virtual Machines',
+            area: '中国北区'
+        },
+        {
+            id: '2017',
+            date: '2017 03-02',
+            service: 'Virtual Machines',
+            area: '中国北区'
+        },
+        {
+            id: '2017',
+            date: '2017 03-02',
+            service: 'Virtual Machines',
+            area: '中国北区'
+        },
+        {
+            id: '2017',
+            date: '2017 03-02',
+            service: 'Virtual Machines',
+            area: '中国北区'
+        },
+        {
+            id: '2017',
+            date: '2017 03-02',
+            service: 'Virtual Machines',
+            area: '中国北区'
+        },
+        {
+            id: '2017',
+            date: '2017 03-02',
+            service: 'Virtual Machines',
+            area: '中国北区'
+        },
+        {
+            id: '2017',
+            date: '2017 03-02',
+            service: 'Virtual Machines',
+            area: '中国北区'
+        },
+        {
+            id: '2017',
+            date: '2017 03-02',
+            service: 'Virtual Machines',
+            area: '中国北区'
+        }]
+    },
+    viewDetail: function(event){
+        var detailId = event.currentTarget.dataset.id;
+        wx.navigateTo({
+            url: '../history-detail/history-detail?id=' + detailId
+        });
+    }
 });
